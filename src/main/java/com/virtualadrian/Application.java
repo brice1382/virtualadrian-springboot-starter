573117df1f65ad1f3ab9e7ddd032981d0d7da9ed
Adrian Pavelescu <Adrian.Pavelescu@gfs.com>
2017-09-27 04:30:40 -0400

    added w2

--- src/main/java/com/virtualadrian/Application.java
+++ src/main/java/com/virtualadrian/Application.java
@@ -4,14 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.virtualadrian.config.ServerMonitorRepositoryConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import com.virtualadrian.config.SwaggerConfig;
 
 @Import({ServerMonitorRepositoryConfiguration.class, SwaggerConfig.class})
 @SpringBootApplication(scanBasePackages={"com.virtualadrian"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 }
+//
+//{
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
